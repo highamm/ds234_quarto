23c390830e25a537be2475ff27dfd492e38c7c8f
Matt Higham <34220951+highamm@users.noreply.github.com>
2023-05-08 08:38:52 -0400

    updated first couple of sections, added exercises, added callouts.

--- _common.R
+++ _common.R
@@ -1,9 +1,13 @@
 # example R options set globally
-options(width = 60)
+options(width = 77,
+        dplyr.print_min = 6,
+        dplyr.print_max = 6)
 
 # example chunk options set globally
 knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
-  warning = FALSE
+  warning = FALSE,
+  message = FALSE,
+  fig.height = 4
   )
